793e991a949afdbd37c0c8a4ab0159b0cafa0b1b
Rebeja Arina <arinarebeja100@gmail.com>
2026-02-17 13:57:02 +0200

    adaugarea paginilor de eroare

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+        <div className = "h-screen flex flex-col items-center justify-center text-primary bg-background">
+            <h1 className="text-6xl">404</h1>
+            <p>
+				Page is not found. Please, check the URL and try again.
+			</p>
+		</div>
+	);
+}
